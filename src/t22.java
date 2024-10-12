8639a8c6230d335a8becca0a3b8b3339e5a0c803
xli9527 <buli_xli@163.com>
2024-10-13 00:31:21 +0800

    new test code -24/10/13

---
+++ src/t22.java
@@ -0,0 +1,15 @@
+public class t22 {
+    private int num;
+
+
+    public void setNum(){
+        this.num = 114514;
+    }
+
+    public int getNum(){
+        setNum();
+        return  this.num;
+    }
+
+
+}
---
+++ src/t22_2.java
@@ -0,0 +1,6 @@
+public class t22_2 {
+    public static void main(String[] args) {
+        t22 gn = new t22();
+        System.out.println(gn.getNum());
+    }
+}
