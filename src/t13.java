9eedd8981b946518f69045324bcc10ef9a738b0b
xli9527 <buli_xli@163.com>
2024-10-13 01:26:20 +0800

    add some annotations

--- src/t13.java
+++ src/t13.java
@@ -1,14 +1,17 @@
 public class t13 {
     public double height;
     public double width;
+//      长宽作为属性
 
     public t13(){
         height=10.5;
         width=10;
+//      在构造方法初始化
     }
 
     public double area(){
         return height*width;
+//      计算面积
     }
 
 
@@ -16,4 +19,5 @@ public class t13 {
         t13 tt =new t13();
         System.out.println(tt.area());
     }
+//    输出
 }
