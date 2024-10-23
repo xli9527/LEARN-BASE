0856b51385d56be77ce86cd0dcdd45f0abfcb973
xli9527 <buli_xli@163.com>
2024-10-23 22:04:04 +0800

    new test code

---
+++ src/cxsj2023/Q8.java
@@ -0,0 +1,42 @@
+package cxsj2023;
+
+import java.util.Scanner;
+
+public class Q8 {
+    public static void main(String[] args) {
+        Scanner scanner=new Scanner(System.in);
+        int t= scanner.nextInt();
+
+        while( t > 0 ){
+            int n= scanner.nextInt();
+            int num = 0;
+            int f = 0;
+            scanner.nextLine();
+            String chars =scanner.nextLine();
+            for (int i = 0; i < n; i++) {
+                char ch = chars.charAt(i*2);
+                if (ch=='C'){
+                    num++;
+                }  else {
+                   if (f > 0){
+                       System.out.print(" ");
+                   } else {
+                       f++;
+                   }
+                   System.out.print(ch);
+                }
+            }
+            for (int i = 0; i < num ; i++) {
+                if(f > 0){
+                    System.out.print(" ");
+                } else {
+                    f++;
+                }
+                System.out.print("A");
+            }
+            System.out.println();
+            t--;
+        }
+        scanner.close();
+    }
+}
