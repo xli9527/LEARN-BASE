3380c65e611693cf541358707a9ded04e7fc202c
xli9527 <buli_xli@163.com>
2024-11-15 00:18:04 +0800

    new code

--- src/cxsj2023_2024_2/Q5.java
+++ src/cxsj2023_2024_2/Q5.java
@@ -2,7 +2,7 @@ package cxsj2023_2024_2;
 
 import java.util.Scanner;
 
-public class Main {
+public class Q5 {
 
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
