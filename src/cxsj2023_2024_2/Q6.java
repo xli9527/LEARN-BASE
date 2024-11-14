f7f8f7c419c3269453dba230b8d679e97d91c894
xli9527 <buli_xli@163.com>
2024-11-15 02:39:22 +0800

    new code

--- src/cxsj2023_2024_2/Q6.java
+++ src/cxsj2023_2024_2/Q6.java
@@ -2,7 +2,7 @@ package cxsj2023_2024_2;
 
 import java.util.Scanner;
 
-public class Main {
+public class Q6 {
 
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
@@ -14,25 +14,30 @@ public class Main {
 
         }
 
-        int min = arr[0];
         int max = arr[0];
 
         for (int i = 0; i < n; i++) {
-
-            if (arr[i] < min) {
-                min = arr[i];
-            }
-
             if (arr[i] > max) {
                 max = arr[i];
             }
         }
 
+        int temp = 0;
+
+        for (int i = 0; i < n-1; i++) {
+            for (int j = 0; j < n-1-i; j++) {
+                if (arr[j] < arr[j+1]){
+                    temp = arr[j];
+                    arr[j] = arr[j+1];
+                    arr[j+1] = temp;
+                }
+            }
+        }
 
         int map[][]=new int[max][n];
 
         for (int i = 0; i < n; i++) {
-            for (int j = min-1; j < max; j++) {
+            for (int j = 0; j < max; j++) {
                 if (j < arr[i]){
                     map[j][i]=1;
                 }
@@ -41,13 +46,7 @@ public class Main {
 
         int answer[] = new int[max];
 
-        for (int i = 0; i < min; i++) {
-            answer[i] = n;
-        }
-
-
-
-        for (int i = min-1; i < max; i++) {
+        for (int i = 0; i < max; i++) {
             int count = 0;
             for (int j = 0; j < n; j++) {
                 if (map[i][j] == 1){
@@ -56,19 +55,6 @@ public class Main {
             }
             answer[i] = count;
         }
-
-        int temp = 0;
-
-        for (int i = 0; i < max-1; i++) {
-            for (int j = 0; j < max-1-i; j++) {
-                if (answer[j] < answer[j+1]){
-                    temp = answer[j];
-                    answer[j] = answer[j+1];
-                    answer[j+1] = temp;
-                }
-            }
-        }
-
 
         System.out.println(max);
         for (int i : answer) {
--- src/cxsj2023_2024_2/Q6_2.java
+++ src/cxsj2023_2024_2/Q6_2.java
@@ -2,7 +2,7 @@ package cxsj2023_2024_2;
 
 import java.util.Scanner;
 
-public class Main {
+public class Q6_2 {
 
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
